d976df2551d65bffbd6e8f1e1fac6516d1e9b0ae
bittboy <bittboy@bittboy-pc>
2019-08-24 12:05:28 -0300

    removed useless printf

--- invoker/invoker.c
+++ invoker/invoker.c
@@ -5,10 +5,9 @@
 int main(int argc, char *argv[]) {
 	char finalString[200]="";
 	snprintf(finalString, sizeof(finalString), "cd %s;%s", argv[1], argv[2]);
-	printf("%s\n",finalString);
 	int ret = system(finalString);
 	if (argc ==0 || ret==-1) {
-
+		printf("ERROR");
 	}
 	execlp("./simplemenu.elf","simplemenu.elf",NULL);
 }
