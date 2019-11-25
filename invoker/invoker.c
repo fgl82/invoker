157d26020fa715b02088a635c2dd1513525395dc
bittboy <bittboy@bittboy-pc>
2019-11-24 22:16:07 -0300

    Fixed for more use cases

--- invoker/invoker.c
+++ invoker/invoker.c
@@ -7,6 +7,19 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+char *getFilePath(char *fileName) {
+	char *retstr;
+	char *lastslash;
+	retstr = malloc(strlen (fileName) + 1);
+	strcpy (retstr, fileName);
+	lastslash = strrchr (retstr, '/');
+//	sleep(2);
+	if (lastslash != NULL) {
+		*lastslash = '\0';
+	}
+	return retstr;
+}
+
 int main(int argc, char *argv[]) {
 	char menuDirectory[100] = "";
 	char *directory=argv[1];
@@ -17,7 +30,7 @@ int main(int argc, char *argv[]) {
 	char *returnTo=argv[6];
 	char *pictureMode=argv[7];
 	int ret=0;
-	int fd;
+//	int fd;
     struct winsize w;
     ioctl(0, TIOCGWINSZ, &w);
 	getcwd(menuDirectory, sizeof(menuDirectory));
@@ -25,13 +38,17 @@ int main(int argc, char *argv[]) {
 	pid_t pid;
 	pid = fork();
 	if (pid == 0 ) {
-		fd = open("/dev/null",O_WRONLY | O_CREAT, 0666);
-		dup2(fd, 1);
-		dup2(fd, 2);
+//		fd = open("/dev/null",O_WRONLY | O_CREAT, 0666);
+//		dup2(fd, 1);
+//		dup2(fd, 2);
 		//it's a section meant for native apps
 		if (executable[0]=='#') {
+//			printf("WTF!\n");
+//			sleep(1);
 			ret = chdir(menuDirectory);
 			if (ret!=-1) {
+//				printf("WTF2!\n");
+//				sleep(1);
 				//Native opk with a desktop file as parameter
 				char *params[10];
 				if (strstr(fileToBeExecutedWithFullPath,"|")!=NULL) {
@@ -47,20 +64,36 @@ int main(int argc, char *argv[]) {
 					params[i]=NULL;
 					ret = execvp("opkrun",params);
 				} else {
+//					printf("WTF3 %s!\n", fileToBeExecutedWithFullPath);
+//					sleep(1);
 					if(strstr(fileToBeExecutedWithFullPath,".opk")) {
 						//it's an opk with just a default desktop file
+//						printf("SIMPLE OPK! %s\n",fileToBeExecutedWithFullPath);
+//						sleep(2);
 						ret = execlp("opkrun","invoker",fileToBeExecutedWithFullPath,NULL);
 					} else {
-						//it's a generic executable file
+						char* dirToSwitch = getFilePath(fileToBeExecutedWithFullPath);
+//						printf("%s\n",dirToSwitch);
+//						sleep(2);
+						chdir(dirToSwitch);
 						ret = execlp(fileToBeExecutedWithFullPath,"invoker",NULL);
 					}
 				}
 			}
 		} else {
-			//Emulation: it's an opk with a rom as a parameter
-			ret=execlp("opkrun","invoker",executable,fileToBeExecutedWithFullPath,NULL);
+			//it's an emulator
+			if (strstr(executable,".opk")) {
+				//an opk emulator
+				ret=execlp("opkrun","invoker",executable,fileToBeExecutedWithFullPath,NULL);
+			} else {
+				//it's an non-opk emulator
+				char localExec[100];
+				strcpy(localExec,"./");
+				strcat(localExec,executable);
+				ret=execlp(localExec,"invoker",fileToBeExecutedWithFullPath,NULL);
+			}
 		}
-		close(fd);
+//		close(fd);
 	} else {
 		wait(0);
 	}
